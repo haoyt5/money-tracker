0df288d9d34a2a49b6bf36daeb4947272947a9a6
Karen HY Tu <karenhaoyitu@gmail.com>
2023-01-29 01:49:37 -0800

    fix: remove unused code

--- server/src/db/Database.ts
+++ server/src/db/Database.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { Expense } from '../models/expense'
+// import { Expense } from '../models/expense'
 export const dbPath: string = 'mongodb://127.0.0.1:27017/money-tracker'
 
 export class Database {
@@ -8,26 +8,16 @@ export class Database {
     constructor() {
         this.connection = null
     }
-    // async connect(cb: Function) {
-    //     try {
-    //         const client = await MongoClient.connect(dbPath)
-    //         this.connection = client.db()
-    //         return cb()
-    //     } catch (error) {
-    //         console.log('[mongodb]' + error)
-    //         return cb(error)
-    //     }
-    // }
     async connect(cb: Function) {
         try {
             const client = await mongoose.connect(dbPath)
             this.connection = client
-            const expense = new Expense({
-                user: 'test',
-                category: 'bills',
-                amount: '35',
-            })
-            await expense.save()
+            // const expense = new Expense({
+            //     user: 'test',
+            //     category: 'bills',
+            //     amount: '35',
+            // })
+            // await expense.save()
             return cb()
         } catch (error) {
             console.log('[mongodb]' + error)
