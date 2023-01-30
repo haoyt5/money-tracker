645df12e5374afb470eff5dd4e5d5b1d7bbd6413
Karen HY Tu <karenhaoyitu@gmail.com>
2023-01-29 22:26:56 -0800

    feat: add PageforwardButton

---
+++ client/src/components/PageBackwardButton.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'reactstrap'
+
+interface PageBackWardButtonProps {
+    from: string
+    pathname: string
+}
+
+const PageBackWardButton: FunctionComponent<PageBackWardButtonProps> = ({
+    from,
+    pathname,
+}) => {
+    return (
+        <div className={`${[pathname === '/' && 'opacity-0']}`}>
+            <Link to={from}>
+                <Button
+                    color="info"
+                    className="font-medium bg-blue-300 hover:bg-white font-semibold border-0 "
+                >
+                    {'＜'}
+                </Button>
+            </Link>
+        </div>
+    )
+}
+
+export default PageBackWardButton
