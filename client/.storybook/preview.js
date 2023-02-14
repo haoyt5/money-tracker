c2565e4bbfebd8feac629be15390d51e259507dd
Karen HY Tu <karenhaoyitu@gmail.com>
2023-02-14 03:07:13 -0800

    init: add css for storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,9 +1,13 @@
+import './css/bootstrap.css'
+import './css/compiled-tailwind.css'
+import './css/compiled-tailwind.min.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
